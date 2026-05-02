f8e9cd95daba01bf308dd421da81f1604d8f6fdb
James Holloway <james.holloway94@gmail.com>
2026-05-02 08:36:56 +0000

    capture rds snapshot completion time in record

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-02 07:23:50 UTC
+# Last sync: 2026-05-02 08:36:56 UTC

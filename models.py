300133a9d7b611252b2efa3957880342485025d2
James Holloway <james.holloway94@gmail.com>
2026-05-02 15:47:22 +0000

    inspect bastion port forward sync for security

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-02 14:38:12 UTC
+# Last sync: 2026-05-02 15:47:22 UTC

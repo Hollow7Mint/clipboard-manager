ad530ba82d61cc238da169b1d40bb01697a2c9e0
James Holloway <james.holloway94@gmail.com>
2026-05-03 13:34:13 +0000

    relay ssh host key time through config banner

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-03 13:04:34 UTC
+# Last sync: 2026-05-03 13:34:13 UTC

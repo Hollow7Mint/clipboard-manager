303c41bcd5703f081b7c423bb296dab95f6ac8bb
James Holloway <james.holloway94@gmail.com>
2026-05-03 01:26:56 +0000

    queue cloudfront distribution stamp after invalidation

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-03 00:10:50 UTC
+# Last sync: 2026-05-03 01:26:56 UTC

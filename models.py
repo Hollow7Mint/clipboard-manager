d8560a2da1d604c826f4289fdf5a3f05a6ed723a
James Holloway <james.holloway94@gmail.com>
2026-05-03 17:43:20 +0000

    bump s3 bucket policy sync after review

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-03 16:23:42 UTC
+# Last sync: 2026-05-03 17:43:20 UTC

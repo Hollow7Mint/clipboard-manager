4928d0115c866278b31c1ce6f13ee393437ea718
James Holloway <james.holloway94@gmail.com>
2026-05-04 19:28:51 +0000

    catalog rds parameter group sync after maintenance

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-04 18:53:21 UTC
+# Last sync: 2026-05-04 19:28:51 UTC

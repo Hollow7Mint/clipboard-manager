d429b30df6ef5d30b5dde3a092e3d73205fa8402
James Holloway <james.holloway94@gmail.com>
2026-05-05 07:15:45 +0000

    keep ecs task sync marker fresh after rollout

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-05 06:49:43 UTC
+# Last sync: 2026-05-05 07:15:45 UTC

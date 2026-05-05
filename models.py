e8c1ea573a8fc9e5c90b5f1edbabfa300080e2f7
James Holloway <james.holloway94@gmail.com>
2026-05-05 05:31:11 +0000

    inspect iam trust policy sync marker for review

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-05 04:27:44 UTC
+# Last sync: 2026-05-05 05:31:11 UTC

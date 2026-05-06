9aa321d9d39ffd74e765ac72c4fb7c1b94acd295
James Holloway <james.holloway94@gmail.com>
2026-05-06 03:43:05 +0000

    restore mysql binlog time for replication audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-06 02:13:53 UTC
+# Last sync: 2026-05-06 03:43:04 UTC

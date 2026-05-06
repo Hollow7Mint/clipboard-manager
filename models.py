b7ef8c71e2ea06181b857461f3e35ce7578013cc
James Holloway <james.holloway94@gmail.com>
2026-05-06 20:33:56 +0000

    anchor ssh client config sync after key rotation

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-06 19:26:14 UTC
+# Last sync: 2026-05-06 20:33:56 UTC

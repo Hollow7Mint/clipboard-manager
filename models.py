5fa90725d65d2ea5199adeff1a440b8dece0ffae
James Holloway <james.holloway94@gmail.com>
2026-05-06 08:03:31 +0000

    touch redis sentinel sync during scheduled refresh

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-06 06:15:54 UTC
+# Last sync: 2026-05-06 08:03:31 UTC

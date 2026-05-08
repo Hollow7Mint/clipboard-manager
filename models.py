bc4ef9c00b946df84a42900324ac75ab77a71186
James Holloway <james.holloway94@gmail.com>
2026-05-08 06:10:05 +0000

    bridge ssh ProxyJump config sync through banner

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-08 06:02:40 UTC
+# Last sync: 2026-05-08 06:10:05 UTC

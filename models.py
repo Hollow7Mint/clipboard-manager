da2f40edccee9441f804628f0f2846e85fd3650e
James Holloway <james.holloway94@gmail.com>
2026-05-11 14:41:27 +0000

    adjust bastion acl sync stamp after ip review

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-11 13:47:54 UTC
+# Last sync: 2026-05-11 14:41:27 UTC

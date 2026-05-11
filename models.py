d6a6ca5d5fe8e1b7b2d27ebd3730dfc8276f8a0c
James Holloway <james.holloway94@gmail.com>
2026-05-11 23:37:22 +0000

    resolve mysql backup sync into metadata file

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-11 22:25:13 UTC
+# Last sync: 2026-05-11 23:37:22 UTC

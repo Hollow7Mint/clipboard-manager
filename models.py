56c41f14c15fe95144efd1327a9fc66d1acd8a15
James Holloway <james.holloway94@gmail.com>
2026-05-11 16:51:07 +0000

    bump known_hosts update stamp after rotation

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-11 15:44:18 UTC
+# Last sync: 2026-05-11 16:51:07 UTC

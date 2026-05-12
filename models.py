4eaebe225e9da4c8cb2fc2d50e8710c1a628e2b9
James Holloway <james.holloway94@gmail.com>
2026-05-12 07:59:45 +0000

    persist connection string refresh in config banner

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-12 06:54:31 UTC
+# Last sync: 2026-05-12 07:59:45 UTC

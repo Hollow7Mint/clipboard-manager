d9b99bb2c12763f143938957e00eff796eef0afd
James Holloway <james.holloway94@gmail.com>
2026-05-14 02:16:25 +0000

    flush ec2 fleet inventory marker after scan

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-14 01:58:37 UTC
+# Last sync: 2026-05-14 02:16:25 UTC

096fce30235f2f11cf2e9e6873b32e4ba8f0a179
James Holloway <james.holloway94@gmail.com>
2026-05-15 19:57:30 +0000

    retain cloudwatch metric filter time for audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-15 18:37:21 UTC
+# Last sync: 2026-05-15 19:57:29 UTC

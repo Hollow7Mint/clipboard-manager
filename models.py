5412aef9efb45af0b0aeeedbdaae969cd53e7b7f
James Holloway <james.holloway94@gmail.com>
2026-05-16 12:20:14 +0000

    adjust redis ttl sync stamp after cache flush

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-16 11:36:15 UTC
+# Last sync: 2026-05-16 12:20:13 UTC

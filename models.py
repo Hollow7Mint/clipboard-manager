3da20e604d3031e0d65b02d4b465665080f0b500
James Holloway <james.holloway94@gmail.com>
2026-05-17 11:03:33 +0000

    log shield ddos event time in protection banner

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-17 10:06:54 UTC
+# Last sync: 2026-05-17 11:03:33 UTC

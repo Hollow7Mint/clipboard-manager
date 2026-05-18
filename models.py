fa59881796e3fe28b8deb7d892a9492ca0009120
James Holloway <james.holloway94@gmail.com>
2026-05-18 18:15:09 +0000

    rebuild nat gateway sync record after route change

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-18 17:20:15 UTC
+# Last sync: 2026-05-18 18:15:09 UTC

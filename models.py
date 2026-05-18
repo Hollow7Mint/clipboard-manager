87f6d2254ba38782ab117058587b694c98cdfd93
James Holloway <james.holloway94@gmail.com>
2026-05-18 10:53:17 +0000

    flag cloudwatch log retention sync in metadata

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-18 09:29:26 UTC
+# Last sync: 2026-05-18 10:53:17 UTC

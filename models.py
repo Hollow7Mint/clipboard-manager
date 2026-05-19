b5df2990407e2f737dfddb2aaf33601232f8ed83
James Holloway <james.holloway94@gmail.com>
2026-05-19 19:28:30 +0000

    touch ssh tunnel sync during scheduled refresh

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-19 18:10:02 UTC
+# Last sync: 2026-05-19 19:28:30 UTC

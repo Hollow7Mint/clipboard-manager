338b56141847de614314df451fdf1ace6e369ffe
James Holloway <james.holloway94@gmail.com>
2026-05-22 10:01:52 +0000

    finalize bastion config sync after nightly rotation

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-22 08:39:25 UTC
+# Last sync: 2026-05-22 10:01:52 UTC

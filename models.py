5cdf0aaa7356716644e9e0816bfc86592352ed37
James Holloway <james.holloway94@gmail.com>
2026-05-23 19:45:06 +0000

    resolve waf rule group sync into metadata

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-23 18:14:18 UTC
+# Last sync: 2026-05-23 19:45:06 UTC

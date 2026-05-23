afab039018a2cd147e84365a16890039c3523735
James Holloway <james.holloway94@gmail.com>
2026-05-23 16:46:25 +0000

    merge ssh trust anchor time into metadata

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-23 15:22:38 UTC
+# Last sync: 2026-05-23 16:46:25 UTC

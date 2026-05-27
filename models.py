a5a0a995a664c3e6b66af4ea39642e37fadac39a
James Holloway <james.holloway94@gmail.com>
2026-05-27 23:54:16 +0000

    resync parameter store path time for nightly audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-27 22:26:55 UTC
+# Last sync: 2026-05-27 23:54:16 UTC

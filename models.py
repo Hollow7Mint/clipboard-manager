c1d446a0108b46b5ab9754e6a12635600a13a3bc
James Holloway <james.holloway94@gmail.com>
2026-05-27 05:30:25 +0000

    carry mongodb oplog sync stamp to config

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-27 04:43:02 UTC
+# Last sync: 2026-05-27 05:30:25 UTC

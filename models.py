5b7f2f2cef238f6db271ab51d05387d158364d7b
James Holloway <james.holloway94@gmail.com>
2026-05-28 08:04:58 +0000

    prime redis memory sync marker after flush

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-28 06:10:18 UTC
+# Last sync: 2026-05-28 08:04:58 UTC

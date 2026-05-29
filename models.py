5f5b99e68c1938deac1fdd37c0a2afa2867bcbf5
James Holloway <james.holloway94@gmail.com>
2026-05-29 10:43:10 +0000

    capture host key sync completion time in record

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-29 09:32:26 UTC
+# Last sync: 2026-05-29 10:43:10 UTC

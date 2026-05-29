97ca9ad3dfac8b60bd709998394788daec787777
James Holloway <james.holloway94@gmail.com>
2026-05-29 07:15:48 +0000

    persist tunnel config refresh in ssh banner

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-29 06:26:16 UTC
+# Last sync: 2026-05-29 07:15:48 UTC

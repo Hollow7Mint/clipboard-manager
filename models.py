cf2c76a70c5bd721bd951be2273fdc94b7226c84
James Holloway <james.holloway94@gmail.com>
2026-05-30 00:53:54 +0000

    lift db replica sync time into banner record

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-29 23:52:16 UTC
+# Last sync: 2026-05-30 00:53:54 UTC

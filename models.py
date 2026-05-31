29501b7d721b948aaf29509c2c16485845f88abb
James Holloway <james.holloway94@gmail.com>
2026-05-31 23:15:58 +0000

    set redis cluster sync stamp in config

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-31 22:48:26 UTC
+# Last sync: 2026-05-31 23:15:58 UTC

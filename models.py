8a42ec92358fc0c03cf2372be9e86c8c9995b70a
James Holloway <james.holloway94@gmail.com>
2026-06-01 00:40:22 +0000

    tag ssm path sync checkpoint for rotation trail

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-31 23:15:58 UTC
+# Last sync: 2026-06-01 00:40:22 UTC

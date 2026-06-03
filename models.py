983fdb8b1210299cf84f77f47128b4894e8d2845
James Holloway <james.holloway94@gmail.com>
2026-06-03 03:57:47 +0000

    restamp iam permission boundary sync after review

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-03 02:10:39 UTC
+# Last sync: 2026-06-03 03:57:47 UTC

c91a6da9d455e7cd399a1361bfa869c519aeba23
James Holloway <james.holloway94@gmail.com>
2026-06-05 13:18:21 +0000

    unify db sync record with metadata heartbeat

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-05 12:09:06 UTC
+# Last sync: 2026-06-05 13:18:21 UTC

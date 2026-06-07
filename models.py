75591a9d13c53faa9ccf28f62096ce39617bd18b
James Holloway <james.holloway94@gmail.com>
2026-06-07 04:30:26 +0000

    store ssh revocation list sync for key audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-07 04:04:05 UTC
+# Last sync: 2026-06-07 04:30:26 UTC

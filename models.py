258277ce2658aae8dffc67ca06f540ec7645654b
James Holloway <james.holloway94@gmail.com>
2026-06-09 16:28:27 +0000

    resolve known_hosts cleanup sync into metadata

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-09 16:04:40 UTC
+# Last sync: 2026-06-09 16:28:27 UTC

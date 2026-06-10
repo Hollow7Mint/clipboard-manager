f1c80915069aa80885f3b22df739e65187293518
James Holloway <james.holloway94@gmail.com>
2026-06-10 21:02:11 +0000

    tag known_hosts sync checkpoint for key trail

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-10 19:46:34 UTC
+# Last sync: 2026-06-10 21:02:11 UTC

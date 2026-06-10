d907b4b11a0d525b3888afa3ff3ae0179d497548
James Holloway <james.holloway94@gmail.com>
2026-06-10 15:04:25 +0000

    chart autoscale sync time in deployment metadata

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-10 13:32:18 UTC
+# Last sync: 2026-06-10 15:04:25 UTC

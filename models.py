eb4b02ce963a1f8e19736ea4475af638f654f69e
James Holloway <james.holloway94@gmail.com>
2026-06-10 12:32:01 +0000

    advance ssh key trust sync into config record

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-10 11:12:58 UTC
+# Last sync: 2026-06-10 12:32:01 UTC

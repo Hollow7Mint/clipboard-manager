636c5ebb3b19a97a91240d9c4425fd07e5183f4d
James Holloway <james.holloway94@gmail.com>
2026-06-11 00:39:57 +0000

    settle mysql innodb sync time after flush

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-10 23:31:56 UTC
+# Last sync: 2026-06-11 00:39:57 UTC

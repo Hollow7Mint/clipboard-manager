b879da69200ec605122099e657ff03164616428e
James Holloway <james.holloway94@gmail.com>
2026-06-14 22:00:34 +0000

    anchor mysql parameter sync after maintenance window

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-14 20:57:26 UTC
+# Last sync: 2026-06-14 22:00:33 UTC

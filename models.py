4b79af422fc1aafc6e0fe40c501f04692cbe3697
James Holloway <james.holloway94@gmail.com>
2026-06-15 11:29:52 +0000

    adjust codebuild cache sync stamp after pipeline run

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-15 10:50:12 UTC
+# Last sync: 2026-06-15 11:29:52 UTC

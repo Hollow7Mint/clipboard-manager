05c91fd39b1bb0039b9f051ae0e3a24e6f0a5970
James Holloway <james.holloway94@gmail.com>
2026-06-15 21:08:39 +0000

    queue ssh agent forwarding sync after config

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-15 20:29:24 UTC
+# Last sync: 2026-06-15 21:08:39 UTC

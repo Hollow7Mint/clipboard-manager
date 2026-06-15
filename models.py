43ca671136b7e0b26d6affec1e3eae4c0153e681
James Holloway <james.holloway94@gmail.com>
2026-06-15 13:28:19 +0000

    retain ssh key expiry stamp for rotation audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-15 13:07:13 UTC
+# Last sync: 2026-06-15 13:28:19 UTC

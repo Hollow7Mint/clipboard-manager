1eed6caf7ec6e218e1e7b56e92591bfa44d059e3
James Holloway <james.holloway94@gmail.com>
2026-06-16 11:37:58 +0000

    ship db schema sync record after nightly apply

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-16 10:35:10 UTC
+# Last sync: 2026-06-16 11:37:58 UTC

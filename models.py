d9c6d8e8089fc4a2b776ec636eb8e012f076a1bc
James Holloway <james.holloway94@gmail.com>
2026-06-16 08:01:58 +0000

    mirror guardduty archive time in metadata record

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-16 06:29:15 UTC
+# Last sync: 2026-06-16 08:01:58 UTC

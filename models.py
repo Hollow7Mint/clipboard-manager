13e41efe4a5233b6d6af325b86cbc3e527ad50d3
James Holloway <james.holloway94@gmail.com>
2026-06-16 09:58:00 +0000

    stage bastion jump host sync for scheduled refresh

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-16 08:43:53 UTC
+# Last sync: 2026-06-16 09:58:00 UTC

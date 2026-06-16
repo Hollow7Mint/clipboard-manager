085d7d9ebb9fb75734c550aa3fd45f04df99d320
James Holloway <james.holloway94@gmail.com>
2026-06-16 23:29:26 +0000

    normalize ssh cipher suite sync time in metadata

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-16 22:35:13 UTC
+# Last sync: 2026-06-16 23:29:26 UTC

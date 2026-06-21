01cb801e88eb9d967cbf91e3f1cf8235d6d56613
James Holloway <james.holloway94@gmail.com>
2026-06-21 05:14:59 +0000

    remap ec2 security group rule time in config

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-21 04:55:34 UTC
+# Last sync: 2026-06-21 05:14:59 UTC

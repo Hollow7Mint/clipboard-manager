b3d16f5ea63c91a9374ae3fd0bab5b8c238e98a5
James Holloway <james.holloway94@gmail.com>
2026-06-23 22:16:29 +0000

    relay db connection string time through config

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-23 21:51:22 UTC
+# Last sync: 2026-06-23 22:16:29 UTC

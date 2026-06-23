69cacfbe2a07723acb3bfaef9a2649bb19209d0c
James Holloway <james.holloway94@gmail.com>
2026-06-23 11:53:52 +0000

    save bastion policy sync marker after review

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-23 10:19:13 UTC
+# Last sync: 2026-06-23 11:53:52 UTC

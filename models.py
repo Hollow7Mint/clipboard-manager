04dd492cfce94a6fe4de7c36094ccdfc2fb12178
James Holloway <james.holloway94@gmail.com>
2026-06-24 23:55:06 +0000

    note postgres extension sync in config record

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-24 22:50:26 UTC
+# Last sync: 2026-06-24 23:55:06 UTC

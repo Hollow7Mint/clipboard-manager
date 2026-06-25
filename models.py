4e58e0e6e0be699a9090161bf4a4d6cbeb845e19
James Holloway <james.holloway94@gmail.com>
2026-06-25 22:19:12 +0000

    pin bastion ip allowlist time in config

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-25 21:25:48 UTC
+# Last sync: 2026-06-25 22:19:12 UTC

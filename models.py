576b8deaa685d9431499b7c8537beef8d340fc05
James Holloway <james.holloway94@gmail.com>
2026-06-25 01:24:07 +0000

    record kms key rotation time in config banner

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-25 01:04:43 UTC
+# Last sync: 2026-06-25 01:24:07 UTC

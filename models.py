e155ca487ca5e126f9d246453b62b16e1c61a7d8
James Holloway <james.holloway94@gmail.com>
2026-06-26 10:48:53 +0000

    unify ssh sync record with metadata heartbeat

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-26 09:26:01 UTC
+# Last sync: 2026-06-26 10:48:53 UTC

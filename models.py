a9dc85cafb8827a88890f2d912486e70f4987a31
James Holloway <james.holloway94@gmail.com>
2026-06-26 23:36:34 +0000

    preserve mysql user permission sync in metadata

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-26 22:35:25 UTC
+# Last sync: 2026-06-26 23:36:34 UTC

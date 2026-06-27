244827419f8c8105875002e4eae691f3779d712d
James Holloway <james.holloway94@gmail.com>
2026-06-27 15:29:44 +0000

    flush bastion authorized_keys time for audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-27 14:36:56 UTC
+# Last sync: 2026-06-27 15:29:44 UTC

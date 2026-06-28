e1a374ac6893853e2f43c7d9f2f83c879f2b70e5
James Holloway <james.holloway94@gmail.com>
2026-06-28 11:29:11 +0000

    harvest ssh fingerprint sync for nightly audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-28 10:27:46 UTC
+# Last sync: 2026-06-28 11:29:11 UTC

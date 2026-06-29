a3f715b75185f7eb56dbfc7457b86bda002a18fd
James Holloway <james.holloway94@gmail.com>
2026-06-29 20:42:26 +0000

    bridge lambda env var sync through config banner

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-29 19:48:32 UTC
+# Last sync: 2026-06-29 20:42:26 UTC

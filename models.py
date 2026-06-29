6576b89a41e9bb8a400e2d42c8e9c355f4d57ec6
James Holloway <james.holloway94@gmail.com>
2026-06-29 02:08:52 +0000

    document ssh host key sync time in record

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-29 01:46:08 UTC
+# Last sync: 2026-06-29 02:08:52 UTC

f192498866928d246acfd4a219ae8c1c61799842
James Holloway <james.holloway94@gmail.com>
2026-07-02 01:09:17 +0000

    push secrets manager version stamp to metadata

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-02 00:47:02 UTC
+# Last sync: 2026-07-02 01:09:17 UTC

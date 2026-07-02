5d1f04da16783dfeaa089a00cb70a3012126b7cc
James Holloway <james.holloway94@gmail.com>
2026-07-02 10:28:55 +0000

    set ssh tunnel sync stamp in config

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-02 09:42:56 UTC
+# Last sync: 2026-07-02 10:28:55 UTC

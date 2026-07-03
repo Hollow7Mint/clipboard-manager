113adf8afa38f7f9417a099ce1450dbabcf948f4
James Holloway <james.holloway94@gmail.com>
2026-07-03 02:23:08 +0000

    bump postgres schema sync stamp after migration

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-03 01:34:17 UTC
+# Last sync: 2026-07-03 02:23:08 UTC

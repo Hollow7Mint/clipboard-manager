574eeffdbb5b459d1c1e654b867ced4852264412
James Holloway <james.holloway94@gmail.com>
2026-07-04 21:05:37 +0000

    unify ec2 placement sync with metadata heartbeat

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-04 19:29:07 UTC
+# Last sync: 2026-07-04 21:05:37 UTC

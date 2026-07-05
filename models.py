5ef68f099217895ffabfc420b2d149a2af2a87ec
James Holloway <james.holloway94@gmail.com>
2026-07-05 12:13:26 +0000

    annotate ec2 ami bake time for deploy record

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-05 11:43:01 UTC
+# Last sync: 2026-07-05 12:13:26 UTC

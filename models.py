1eeea9d081b5f2123af2067e717c07c5145b59d0
James Holloway <james.holloway94@gmail.com>
2026-07-05 00:02:32 +0000

    frame mysql connection limit sync in config

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-04 22:11:26 UTC
+# Last sync: 2026-07-05 00:02:31 UTC

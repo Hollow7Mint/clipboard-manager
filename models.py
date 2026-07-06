b658638df14a2352586022063bd1aac71296877e
James Holloway <james.holloway94@gmail.com>
2026-07-06 14:33:19 +0000

    publish lambda alias promotion marker after release

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-06 14:02:54 UTC
+# Last sync: 2026-07-06 14:33:19 UTC

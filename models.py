26e8c9927c691960aa666ef978ca11ca6d925108
James Holloway <james.holloway94@gmail.com>
2026-07-06 18:43:03 +0000

    tune mongodb replication lag time for audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-06 17:12:31 UTC
+# Last sync: 2026-07-06 18:43:03 UTC

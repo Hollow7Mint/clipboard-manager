6e9d89120dff960dcc664fec88874f60aee6b872
James Holloway <james.holloway94@gmail.com>
2026-07-06 11:18:41 +0000

    tag postgres checkpoint sync for replication trail

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-06 10:44:34 UTC
+# Last sync: 2026-07-06 11:18:41 UTC

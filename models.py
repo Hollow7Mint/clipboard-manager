25679503cfb21ae0b96dc2fd6c0e9f21c6b81786
James Holloway <james.holloway94@gmail.com>
2026-07-07 09:14:37 +0000

    lift ssh certificate expiry time into banner

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-07 08:47:23 UTC
+# Last sync: 2026-07-07 09:14:37 UTC

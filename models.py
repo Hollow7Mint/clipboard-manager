6815c12c1b0997b283aa937d17640a11a3acaeb2
James Holloway <james.holloway94@gmail.com>
2026-07-11 08:13:30 +0000

    restore ssh public key time for rotation audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-11 07:17:27 UTC
+# Last sync: 2026-07-11 08:13:30 UTC

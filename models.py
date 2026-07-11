b6af41b24f53e54ab88f01a5b2501fe2b8838006
James Holloway <james.holloway94@gmail.com>
2026-07-11 12:08:57 +0000

    frame ssm document sync time in config record

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-11 10:23:19 UTC
+# Last sync: 2026-07-11 12:08:57 UTC

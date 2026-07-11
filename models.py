1fc54dc904a40e389585a67705cbb44a9040ceaf
James Holloway <james.holloway94@gmail.com>
2026-07-11 10:23:19 +0000

    load mysql slow query log sync from pipeline

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-11 09:52:38 UTC
+# Last sync: 2026-07-11 10:23:19 UTC

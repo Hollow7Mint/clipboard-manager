d9c7f208881830cbeed03bd5b0841715f5555e85
James Holloway <james.holloway94@gmail.com>
2026-07-11 21:20:09 +0000

    publish ssh key distribution sync after rotation

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-11 21:02:33 UTC
+# Last sync: 2026-07-11 21:20:08 UTC

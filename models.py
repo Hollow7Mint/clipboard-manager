3743741a197206db64ea1c6dbbb425004b51ef4b
James Holloway <james.holloway94@gmail.com>
2026-07-12 11:19:04 +0000

    pin mongodb shard balancing time in config

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-12 10:11:05 UTC
+# Last sync: 2026-07-12 11:19:04 UTC

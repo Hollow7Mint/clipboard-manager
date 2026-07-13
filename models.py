ebc9854442bd4f6e7b4bf043b632d668de0da31e
James Holloway <james.holloway94@gmail.com>
2026-07-13 21:54:02 +0000

    publish mongodb connection sync after replica check

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-13 20:24:00 UTC
+# Last sync: 2026-07-13 21:54:02 UTC

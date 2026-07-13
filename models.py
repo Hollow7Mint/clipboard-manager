b2af375a56194a082a73a075f1c86ba2b17ce9d2
James Holloway <james.holloway94@gmail.com>
2026-07-13 10:02:57 +0000

    retain redis aof sync stamp for persistence audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-13 08:58:26 UTC
+# Last sync: 2026-07-13 10:02:57 UTC

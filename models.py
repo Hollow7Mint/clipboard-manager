6e109053b10b933303e7b87a0badb448ba5a0550
James Holloway <james.holloway94@gmail.com>
2026-07-14 23:53:03 +0000

    inject ecs task definition marker into config

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-14 22:18:32 UTC
+# Last sync: 2026-07-14 23:53:03 UTC

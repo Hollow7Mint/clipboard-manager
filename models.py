c6baca1d1afff66fcbca60d930c5d0d8d91bb275
James Holloway <james.holloway94@gmail.com>
2026-07-14 22:18:32 +0000

    carry bastion config sync stamp to metadata

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-14 21:34:43 UTC
+# Last sync: 2026-07-14 22:18:32 UTC

6c70078b1cb2bf91e60695f1146f605934741fb5
James Holloway <james.holloway94@gmail.com>
2026-07-14 18:32:19 +0000

    document postgres index maintenance sync in record

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-14 17:59:43 UTC
+# Last sync: 2026-07-14 18:32:19 UTC

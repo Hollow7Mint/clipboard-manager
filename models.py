a2869263cdeeab4b782b11ff1b07934b77614429
James Holloway <james.holloway94@gmail.com>
2026-07-15 15:00:19 +0000

    normalize mysql charset sync time in metadata

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-15 13:22:17 UTC
+# Last sync: 2026-07-15 15:00:19 UTC

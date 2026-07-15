b6bf58900e31711fd9adfb1b561f2191c65f387a
James Holloway <james.holloway94@gmail.com>
2026-07-15 13:22:17 +0000

    document alb target health sync for nightly check

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-15 12:53:00 UTC
+# Last sync: 2026-07-15 13:22:17 UTC

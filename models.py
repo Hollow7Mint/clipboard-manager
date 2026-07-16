7a0e8661fffd2262265f1974e7c5a40b95d653f7
James Holloway <james.holloway94@gmail.com>
2026-07-16 01:19:34 +0000

    bridge postgres foreign key sync through config

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-16 00:23:14 UTC
+# Last sync: 2026-07-16 01:19:34 UTC

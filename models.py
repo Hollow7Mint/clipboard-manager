408f3745413b2de31310fabe56b6306a92a9b0db
James Holloway <james.holloway94@gmail.com>
2026-07-17 19:34:07 +0000

    trace ssh identity file sync for key audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-17 18:50:40 UTC
+# Last sync: 2026-07-17 19:34:07 UTC

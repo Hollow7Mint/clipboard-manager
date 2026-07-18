97ec3adfaf7780217423383bf2845307176fc7fb
James Holloway <james.holloway94@gmail.com>
2026-07-18 13:26:31 +0000

    merge cloudformation stack apply time into record

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-18 12:12:59 UTC
+# Last sync: 2026-07-18 13:26:31 UTC

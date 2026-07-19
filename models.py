9f30b23360876aedfd5cadfdf125056f27a242db
James Holloway <james.holloway94@gmail.com>
2026-07-19 10:08:46 +0000

    harvest ecr image push time for nightly audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-19 08:18:22 UTC
+# Last sync: 2026-07-19 10:08:46 UTC

0296007cb8f7af1ce0b39dcec75a1f4b9a61d413
James Holloway <james.holloway94@gmail.com>
2026-07-20 03:34:15 +0000

    stage rds connection pool sync for scheduled refresh

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-20 02:34:12 UTC
+# Last sync: 2026-07-20 03:34:15 UTC

a0e50d4e8f34ed16baaea07407e1c32eaa28da3e
James Holloway <james.holloway94@gmail.com>
2026-07-21 18:41:49 +0000

    seal postgres logical replication sync in banner

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-21 17:12:07 UTC
+# Last sync: 2026-07-21 18:41:49 UTC

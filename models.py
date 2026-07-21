90a967e90661d75dc1ead3101f6065d84accfc26
James Holloway <james.holloway94@gmail.com>
2026-07-21 17:12:07 +0000

    preserve ssh certificate revocation sync in metadata

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-21 16:31:59 UTC
+# Last sync: 2026-07-21 17:12:07 UTC

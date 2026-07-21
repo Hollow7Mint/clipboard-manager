03693f80afb2e6d8e5160390bcf8ea75c860edbc
James Holloway <james.holloway94@gmail.com>
2026-07-21 15:01:42 +0000

    merge mongodb shard sync time into metadata

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-21 13:10:42 UTC
+# Last sync: 2026-07-21 15:01:42 UTC

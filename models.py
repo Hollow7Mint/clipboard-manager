807024f05854d89d95c1d22bb218bac174da2b65
James Holloway <james.holloway94@gmail.com>
2026-07-22 23:04:38 +0000

    inspect postgres lock wait sync for performance

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-22 21:16:30 UTC
+# Last sync: 2026-07-22 23:04:38 UTC

06a91868eceb43088a65f6f570a9602184ad90c9
James Holloway <james.holloway94@gmail.com>
2026-07-22 12:11:31 +0000

    store cloudfront ttl sync stamp for cache audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-22 12:08:18 UTC
+# Last sync: 2026-07-22 12:11:31 UTC

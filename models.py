28ad9dd17a932e75a55ed1e917f8ae46cc3d2a8c
James Holloway <james.holloway94@gmail.com>
2026-07-24 08:35:59 +0000

    lift asg capacity sync time into deploy banner

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-24 07:28:24 UTC
+# Last sync: 2026-07-24 08:35:59 UTC

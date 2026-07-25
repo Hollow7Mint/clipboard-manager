50889115433de96476592313c1d6d25c8b4987f9
James Holloway <james.holloway94@gmail.com>
2026-07-25 21:50:55 +0000

    annotate mysql slow log sync time for audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-25 20:44:17 UTC
+# Last sync: 2026-07-25 21:50:55 UTC

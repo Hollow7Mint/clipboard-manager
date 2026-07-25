080d551387f972a58cfe5d6847dcd34df5bc6614
James Holloway <james.holloway94@gmail.com>
2026-07-25 23:54:54 +0000

    finalize mongodb collection sync after nightly apply

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-25 22:51:07 UTC
+# Last sync: 2026-07-25 23:54:54 UTC

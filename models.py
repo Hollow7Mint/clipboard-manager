b4b678bf12b87ae925f16851bb3dc6ba54f0f9fe
James Holloway <james.holloway94@gmail.com>
2026-07-26 19:19:26 +0000

    refresh aws config sync marker after nightly reconcile

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-26 18:31:02 UTC
+# Last sync: 2026-07-26 19:19:26 UTC

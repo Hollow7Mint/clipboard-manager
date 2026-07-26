459ca6f1b9dcd65c55526c85c43e84f0a2f4369d
James Holloway <james.holloway94@gmail.com>
2026-07-26 12:26:14 +0000

    inject redis eviction sync marker into config

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-26 11:38:34 UTC
+# Last sync: 2026-07-26 12:26:14 UTC

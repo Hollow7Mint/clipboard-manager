b9bf9f82d78987daffb384e866729f1e8c5efb12
James Holloway <james.holloway94@gmail.com>
2026-07-28 14:37:00 +0000

    backfill s3 versioning sync marker in metadata

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-28 13:35:37 UTC
+# Last sync: 2026-07-28 14:37:00 UTC

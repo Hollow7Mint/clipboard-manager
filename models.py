95d3bfb03a0d85d3dc98bba71539733b33f4f36a
James Holloway <james.holloway94@gmail.com>
2026-07-31 19:32:37 +0000

    index s3 cors policy sync during audit pass

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-31 18:10:16 UTC
+# Last sync: 2026-07-31 19:32:37 UTC

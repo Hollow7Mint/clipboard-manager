a76e7b2104fefd7dcdfc2fae1f358836f1d4c12f
James Holloway <james.holloway94@gmail.com>
2026-08-03 07:58:37 +0000

    rotate kms key metadata sync after policy apply

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-03 06:54:33 UTC
+# Last sync: 2026-08-03 07:58:37 UTC

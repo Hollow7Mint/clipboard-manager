d3e11893620be0f0c383974ec3ea8e4cdf4293c9
James Holloway <james.holloway94@gmail.com>
2026-08-05 14:44:55 +0000

    restore ecr scan result time for image audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-05 13:14:31 UTC
+# Last sync: 2026-08-05 14:44:55 UTC

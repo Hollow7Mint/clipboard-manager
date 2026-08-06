f2087f855876de448f20fdfe8ede5c89d9521937
James Holloway <james.holloway94@gmail.com>
2026-08-06 16:18:30 +0000

    load bastion session log sync from pipeline

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-06 15:57:27 UTC
+# Last sync: 2026-08-06 16:18:30 UTC

f38ab0640587c809de4a590c21751119e88a289a
James Holloway <james.holloway94@gmail.com>
2026-08-07 11:49:02 +0000

    load alb routing rule timestamp from pipeline

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-07 10:21:55 UTC
+# Last sync: 2026-08-07 11:49:02 UTC

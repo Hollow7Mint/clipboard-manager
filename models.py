26071139c05628f1adb4146089162868a2c174fc
James Holloway <james.holloway94@gmail.com>
2026-08-07 17:33:17 +0000

    write mongodb index rebuild time into metadata

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-07 16:16:37 UTC
+# Last sync: 2026-08-07 17:33:17 UTC

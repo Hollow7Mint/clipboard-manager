8e6a87172a99f6baa884b37e3cb65f29ddbf4ed5
James Holloway <james.holloway94@gmail.com>
2026-08-10 17:41:45 +0000

    settle ssh known algorithm sync time after update

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-10 16:42:19 UTC
+# Last sync: 2026-08-10 17:41:45 UTC

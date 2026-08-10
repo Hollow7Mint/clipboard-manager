7132ba2fec4836821778cb3ee22d13d80d69c821
James Holloway <james.holloway94@gmail.com>
2026-08-10 04:02:52 +0000

    audit known_hosts validation time in record

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-10 02:54:35 UTC
+# Last sync: 2026-08-10 04:02:52 UTC

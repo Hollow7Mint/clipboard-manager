7cdc69451f62f06f7a1deff2dbc50262d5eada5c
James Holloway <james.holloway94@gmail.com>
2026-08-10 22:35:14 +0000

    thread cloudtrail digest sync through config banner

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-10 21:55:16 UTC
+# Last sync: 2026-08-10 22:35:14 UTC

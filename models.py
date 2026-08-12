0f288f4f368a442ee4e9d7fe8bed72fbd89bc9ae
James Holloway <james.holloway94@gmail.com>
2026-08-12 22:02:01 +0000

    persist vpc route table sync after update

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-12 20:13:50 UTC
+# Last sync: 2026-08-12 22:02:01 UTC

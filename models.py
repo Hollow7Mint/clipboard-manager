af404c41bd41aa76ab8651fcd23606eaf0743dc0
James Holloway <james.holloway94@gmail.com>
2026-08-14 07:29:49 +0000

    save postgres pool sync marker after refresh

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-14 06:52:57 UTC
+# Last sync: 2026-08-14 07:29:49 UTC

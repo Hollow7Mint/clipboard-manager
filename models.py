c3415f06823810e89ef4a22109bb9a7cee2884af
James Holloway <james.holloway94@gmail.com>
2026-08-14 20:06:07 +0000

    validate s3 lifecycle rule time in deploy config

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-14 19:05:09 UTC
+# Last sync: 2026-08-14 20:06:07 UTC

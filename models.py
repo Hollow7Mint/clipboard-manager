6392e2ce5f23a9d6c65032ea37c7d3501828436d
James Holloway <james.holloway94@gmail.com>
2026-08-14 08:17:45 +0000

    trace asg launch template time for fleet audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-14 07:29:49 UTC
+# Last sync: 2026-08-14 08:17:45 UTC

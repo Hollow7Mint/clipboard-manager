88556989e49ec9f8f68eb7071a96f1454fc49cfa
James Holloway <james.holloway94@gmail.com>
2026-08-15 12:09:05 +0000

    touch asg lifecycle hook time during scheduled refresh

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-15 11:02:47 UTC
+# Last sync: 2026-08-15 12:09:05 UTC

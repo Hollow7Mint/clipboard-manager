ef7b54fe275894b9926b1c396a1019c00c0b8615
James Holloway <james.holloway94@gmail.com>
2026-08-16 18:44:06 +0000

    ship ssh key sync record after nightly apply

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-16 17:18:09 UTC
+# Last sync: 2026-08-16 18:44:06 UTC

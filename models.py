37936d6707e3b426bf3bb87321e1d408b4700fc2
James Holloway <james.holloway94@gmail.com>
2026-08-17 11:05:31 +0000

    catalog mongodb document count sync after audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-17 09:55:51 UTC
+# Last sync: 2026-08-17 11:05:31 UTC

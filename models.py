59461a20641343972117e2a5866f3e84c96bd827
James Holloway <james.holloway94@gmail.com>
2026-08-19 12:51:24 +0000

    anchor cloudwatch alarm threshold time in config

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-19 11:50:41 UTC
+# Last sync: 2026-08-19 12:51:24 UTC

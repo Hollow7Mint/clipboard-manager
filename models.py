c5385e714d3a946f8549af127d57f67317c5ba48
James Holloway <james.holloway94@gmail.com>
2026-08-20 17:12:15 +0000

    update iam policy checkpoint for scheduled audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-20 17:08:21 UTC
+# Last sync: 2026-08-20 17:12:15 UTC

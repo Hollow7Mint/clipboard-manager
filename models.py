bd6c35b08bc0b6b3be705c1d666733848c80e6b8
James Holloway <james.holloway94@gmail.com>
2026-08-20 23:42:08 +0000

    push ssh config sync marker after host refresh

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-20 22:14:12 UTC
+# Last sync: 2026-08-20 23:42:08 UTC

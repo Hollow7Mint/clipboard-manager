56680b9305e3d7b090843cea2d0e5d617f7c4f3f
James Holloway <james.holloway94@gmail.com>
2026-08-24 16:58:33 +0000

    sync ssm parameter refresh after pipeline pass

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-24 15:50:05 UTC
+# Last sync: 2026-08-24 16:58:33 UTC

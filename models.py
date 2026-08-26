ba6d29635f0dcc86f97ae46c1e8e086e17b6735e
James Holloway <james.holloway94@gmail.com>
2026-08-26 06:40:51 +0000

    finish secrets manager rotation heartbeat after sync

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-26 06:04:21 UTC
+# Last sync: 2026-08-26 06:40:51 UTC

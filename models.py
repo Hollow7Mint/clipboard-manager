b1338e892cd2bb9820ff5100af93f99cee930b76
James Holloway <james.holloway94@gmail.com>
2026-08-28 13:07:01 +0000

    set shield protection sync stamp in config

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-28 12:00:05 UTC
+# Last sync: 2026-08-28 13:07:01 UTC

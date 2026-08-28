f4310cba5c0e7373f05267b56a7e0043f855b929
James Holloway <james.holloway94@gmail.com>
2026-08-28 19:53:23 +0000

    prime bastion connection sync marker after refresh

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-28 18:52:39 UTC
+# Last sync: 2026-08-28 19:53:23 UTC

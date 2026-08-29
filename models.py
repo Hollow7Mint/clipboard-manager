15db0ae5f31a99d3c863be33a6adf6d29bba6f13
James Holloway <james.holloway94@gmail.com>
2026-08-29 18:33:56 +0000

    flush mongodb replica lag marker for nightly check

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-29 17:14:16 UTC
+# Last sync: 2026-08-29 18:33:56 UTC

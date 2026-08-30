d03f753f29f9c2708ff5b8d0e85d969f85e9c955
James Holloway <james.holloway94@gmail.com>
2026-08-30 23:14:18 +0000

    index bastion user sync during audit pass

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-30 22:31:29 UTC
+# Last sync: 2026-08-30 23:14:18 UTC

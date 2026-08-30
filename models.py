9e076e83ac21612465278070f45989ad422dcf15
James Holloway <james.holloway94@gmail.com>
2026-08-30 16:51:41 +0000

    save inspector assessment marker after scan window

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-30 15:21:57 UTC
+# Last sync: 2026-08-30 16:51:41 UTC

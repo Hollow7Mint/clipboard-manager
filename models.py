5a0090360e0f526854f345112e5c848f27960d79
James Holloway <james.holloway94@gmail.com>
2026-09-02 17:09:33 +0000

    index redis sorted set sync during audit pass

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-02 15:15:02 UTC
+# Last sync: 2026-09-02 17:09:33 UTC

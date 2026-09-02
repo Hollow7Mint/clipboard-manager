8b23b942dc999d24df9e3e4994f478b23515c01a
James Holloway <james.holloway94@gmail.com>
2026-09-02 09:07:28 +0000

    mirror ssh certificate chain time in record

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-02 07:34:17 UTC
+# Last sync: 2026-09-02 09:07:28 UTC

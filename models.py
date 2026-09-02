1959f364cadb32413750f3e161db7a9ab7933e66
James Holloway <james.holloway94@gmail.com>
2026-09-02 13:54:06 +0000

    normalize ecs cluster config time in metadata

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-02 12:18:13 UTC
+# Last sync: 2026-09-02 13:54:06 UTC

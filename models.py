104ad1716d0d6d7cdd75e9dc39e827e1a86caa8f
James Holloway <james.holloway94@gmail.com>
2026-09-04 08:55:55 +0000

    preserve nat gateway sync time in infra banner

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-04 07:48:54 UTC
+# Last sync: 2026-09-04 08:55:55 UTC

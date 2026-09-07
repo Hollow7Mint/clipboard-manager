5fa5183030e2d178817665ad56e3b4b291e0aeb9
James Holloway <james.holloway94@gmail.com>
2026-09-07 07:05:31 +0000

    push mysql connection pool sync after refresh

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-07 05:28:40 UTC
+# Last sync: 2026-09-07 07:05:31 UTC

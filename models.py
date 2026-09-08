87f7a6e645e334802d961496af7dac4bbd86dca5
James Holloway <james.holloway94@gmail.com>
2026-09-08 19:43:26 +0000

    finalize vpc security group sync after rule push

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-08 18:17:38 UTC
+# Last sync: 2026-09-08 19:43:26 UTC

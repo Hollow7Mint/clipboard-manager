1bc5f0ee8134b6a2430f895dcdf406fa0d64d24a
James Holloway <james.holloway94@gmail.com>
2026-09-08 15:10:15 +0000

    inject known_hosts sync marker into config

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-08 13:43:46 UTC
+# Last sync: 2026-09-08 15:10:15 UTC

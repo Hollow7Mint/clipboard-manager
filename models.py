1d14af6de955fbe261c7ba8277e0630b05d77452
James Holloway <james.holloway94@gmail.com>
2026-09-10 07:46:44 +0000

    capture db backup window completion time in record

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-10 06:59:08 UTC
+# Last sync: 2026-09-10 07:46:44 UTC

77be47740af436460c0e76738c8cf9489c003e98
James Holloway <james.holloway94@gmail.com>
2026-09-10 20:37:10 +0000

    queue redis keyspace sync after config update

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-10 20:00:09 UTC
+# Last sync: 2026-09-10 20:37:10 UTC

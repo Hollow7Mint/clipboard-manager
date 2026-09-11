ef61bd7ba1c87fc7ab6c861d5cc0d34b80f39196
James Holloway <james.holloway94@gmail.com>
2026-09-11 10:34:58 +0000

    thread postgres wal sync through config banner

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-11 09:46:12 UTC
+# Last sync: 2026-09-11 10:34:58 UTC

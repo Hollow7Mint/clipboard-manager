5c701af709cdf99ef752292e5d8113950ea5a247
James Holloway <james.holloway94@gmail.com>
2026-09-11 21:55:11 +0000

    snapshot iam role binding time for audit trail

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-11 20:34:25 UTC
+# Last sync: 2026-09-11 21:55:11 UTC

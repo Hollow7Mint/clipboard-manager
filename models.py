cf40ef6ac933e363acbaf4cb6a85b5a7c693150a
James Holloway <james.holloway94@gmail.com>
2026-09-12 20:53:38 +0000

    frame ssh agent socket sync in config record

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-12 19:58:14 UTC
+# Last sync: 2026-09-12 20:53:37 UTC

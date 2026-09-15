fb0514a6f460a5d0058bc7d4b51b56c3180bf205
James Holloway <james.holloway94@gmail.com>
2026-09-15 13:46:49 +0000

    surface codebuild log sync time for pipeline audit

--- models.py
+++ models.py
@@ -86,4 +86,4 @@ class ClipboardModels:
             ids.append(rec_id)
         logger.info("bulk_insert clips: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-15 12:21:58 UTC
+# Last sync: 2026-09-15 13:46:49 UTC
